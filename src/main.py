1a1a4e0e137c01a358e3b0fd91357eac30197112
pmateosp <pablo.mateos@inteliatec.com>
2024-08-09 14:20:53 +0200

    Refactor main.py to use Translator class for tool instantiation and listening

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import logging
 from multiprocessing import freeze_support
 
 # PROJECT FILES
-from src.engine.translator import instantiate_tools, play, trans3, listen
+from src.engine.translator import Translator
 
 
 def config_args(parser):
@@ -14,9 +14,10 @@ def config_args(parser):
 
 def run_main(args: argparse.Namespace, logger: logging.Logger, config: configparser.ConfigParser) -> int:
     # Instantiate tools translator
-    recognizer, microphone = instantiate_tools(logger)
-
-    # Start listening with asyncio
-    listen(recognizer=recognizer, logger=logger)
+    translator = Translator(logger=logger)
+    if translator.recognizer and translator.microphone:
+        # Start listening with the recognizer
+        translator.listen()
+    return 0
 
 
