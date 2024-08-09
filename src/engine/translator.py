0360d2490da06e5b8a339a66b0238b1ae7e7cfae
pmateosp <pablo.mateos@inteliatec.com>
2024-08-09 14:21:15 +0200

    refactor: Use Translator class for tool instantiation and listening

--- src/engine/translator.py
+++ src/engine/translator.py
@@ -1,44 +1,70 @@
 import logging
+from concurrent.futures import ThreadPoolExecutor
+
 from speech_recognition import Recognizer, Microphone
 from mtranslate import translate
 import pyttsx3
+import threading
 
 
-def instantiate_tools(logger: logging):
-    try:
-        return Recognizer(), Microphone()
-    except OSError:
-        logger.error("No device Audio Available or Incompatible")
-        return exit(0)
+class Translator:
+    def __init__(self, logger):
+        self.recognizer = Recognizer()
+        self.microphone = Microphone()
+        self.logger = logger
+        self.translation_cache = {}
+        self.engine = pyttsx3.init()
+        self._setup_engine()
+        self.executor = ThreadPoolExecutor(max_workers=5)
 
+    def _setup_engine(self):
+        voices = self.engine.getProperty("voices")
+        if voices:
+            # Intenta encontrar una voz en inglés
+            selected_voice = next((voice.id for voice in voices if 'en' in voice.languages), None)
+            self.engine.setProperty("voice", selected_voice if selected_voice else voices[0].id)
+        else:
+            raise ValueError("No voices available or 'voices' is not a valid list.")
 
-def trans3(string, lan):
-    trad = translate(string, lan)
-    return trad
+    def instantiate_tools(self):
+        try:
+            self.recognizer.pause_threshold = 0.5  # Reduce el tiempo de pausa
+            self.recognizer.energy_threshold = 300  # Ajusta el umbral de energía
+            self.recognizer.dynamic_energy_threshold = True  # Ajuste dinámico del umbral de energía
+        except OSError as e:
+            self.logger.error("No Audio Device Available or Incompatible: %s", str(e))
+            exit(0)
 
+    def trans3(self, string, lan):
+        try:
+            if string not in self.translation_cache:
+                self.translation_cache[string] = translate(string, lan)
+            return self.translation_cache[string]
+        except Exception as e:
+            self.logger.error(f"Translation error: {e}")
+            return string  # Devuelve el texto original si falla la traducción
 
-def play(translated_text):
-    engine = pyttsx3.init()
-    voices = engine.getProperty("voices")
-    engine.setProperty("voice", voices[2].id)
-    engine.say(translated_text, "en")
-    engine.runAndWait()
+    def play(self, translated_text):
+        self.engine.say(translated_text)
+        self.engine.runAndWait()
 
+    def split_and_play(self, translated_text):
+        sentences = translated_text.split('. ')
+        for sentence in sentences:
+            self.play(sentence)
 
-def listen(recognizer: Recognizer, logger: logging):
-    while True:
-        with Microphone() as source:
-            print("Hello, I’m your translator")
-            recognizer.pause_threshold = 1
-            audio = recognizer.listen(source)
-            try:
-                text = recognizer.recognize_google(audio, language="es-ES")
-                print("You said: ", text)
-                translated_text = trans3(text, "en")
-                print({translated_text})
-                play(translated_text)
-            except Exception as e:
-                logger.error("Error: ", str(e))
+    def recognize_and_translate(self, recognizer, audio):
+        try:
+            text = recognizer.recognize_google(audio, language="es-ES")
+            self.logger.info(f"You said: {text}")
+            translated_text = self.trans3(text, "en")
+            self.logger.info(f"Translated: {translated_text}")
+            self.split_and_play(translated_text)
+        except Exception as e:
+            self.logger.error(f"Error: {e}")
 
-
-
+    def listen(self):
+        while True:
+            with self.microphone as source:
+                audio = self.recognizer.listen(source, phrase_time_limit=5)
+                self.executor.submit(self.recognize_and_translate, self.recognizer, audio)
